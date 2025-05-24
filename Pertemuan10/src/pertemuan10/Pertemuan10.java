9a2dc4a06218f7acb82237c5f704bba8a1099ef8
OOP-K <arthawebid@gmail.com>
2025-05-24 12:02:33 +0800

    up pertemuan 10

---
+++ Pertemuan10/src/pertemuan10/Pertemuan10.java
@@ -0,0 +1,17 @@
+package pertemuan10;
+
+/**
+ *
+ * @author Made Artha
+ * TGL: 24 Mei 2025
+ */
+public class Pertemuan10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        dbkoneksi cnn = new dbkoneksi();
+    }
+    
+}
